64af226db038cda6a7e5bc1992ba44ac8ec3bcbc
duytri <duytri@PTNHTTT07>
2016-06-20 17:22:24 +0700

    Version 0.1

---
+++ MongoDB2Files/build.sbt
@@ -0,0 +1,19 @@
+import sbt._
+import Keys._
+import sbtassembly.AssemblyPlugin.autoImport._
+
+name := "MongoDB2Files"
+version := "0.1-SNAPSHOT"
+organization := "uit.islab"
+scalaVersion := "2.10.6"
+
+libraryDependencies += "org.mongodb.scala" %% "mongo-scala-driver" % "1.1.1"
+
+unmanagedBase <<= baseDirectory { base => base / "libs" }
+unmanagedResourceDirectories in Compile += baseDirectory.value / "src" / "main"
+excludeFilter in unmanagedResourceDirectories := "*.scala"
+
+scalacOptions += "-deprecation"
+assemblyJarName in assembly := name.value+"-"+version.value+".jar"
+mainClass in assembly := Some("main.scala.MongoDB2Files")
+test in assembly := {}
--- MongoDB2Files/src/main/scala/MongoDB2Files.scala
+++ MongoDB2Files/src/main/scala/MongoDB2Files.scala
@@ -1,7 +1,43 @@
 package main.scala
 
+import org.bson.Document
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.MongoClient
+import org.mongodb.scala.MongoDatabase
+import org.mongodb.scala.MongoCollection
+import java.io.File
+import java.io.BufferedWriter
+import java.io.FileWriter
+
+import main.scala.Helpers._
+
 object MongoDB2Files {
+  /*
+   * args[0]: value of "subject"
+   * args[1]: output directory (/home/duytri/Desktop/testMongo/)
+   * */
   def main(args: Array[String]): Unit = {
-    
+    val mongoClient: MongoClient = MongoClient("mongodb://PTNHTTT07:27017")
+
+    val database: MongoDatabase = mongoClient.getDatabase("allvnexpress")
+    val collection: MongoCollection[Document] = database.getCollection("all")
+    val data = collection.find(equal("subject", args(0))).projection(exclude("_id", "title", "link", "subject"))
+    var iFileName = 0
+    println("No. of doc: " + collection.count.headResult())
+    //data.first().printResults()
+    data.results.foreach { doc =>
+      {
+        val content = doc.head._2.asString.getValue
+        println(content)
+        val file = new File(args(1) + iFileName)
+        val bw = new BufferedWriter(new FileWriter(file, true))
+        bw.flush()
+        bw.write(content)
+        bw.close()
+        iFileName = iFileName + 1
+      }
+    }
+    mongoClient.close()
   }
 }
